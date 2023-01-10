ab53057acdfa20e169138e9d0ad7bb88a596facb
Lachlan Hurst <lachlan.hurst@gmail.com>
2023-01-10 22:54:05 +1100

    added github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vesc-log-video/',  // the repo name, this is needed to deploy with GitHub pages
   plugins: [vue()],
 })
